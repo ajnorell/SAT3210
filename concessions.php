80ae196aa17299a41f618b0c63b44c737c3da78d
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-11-29 13:21:43 -0500

    update 161

--- concessions.php
+++ concessions.php
@@ -3,7 +3,7 @@
 include_once("config.php");
 
 // Fetch contacts (in descending order)
-$result = mysqli_query($mysqli, "SELECT * FROM concessions GROUP BY con_type"); 
+$result = mysqli_query($mysqli, "SELECT * FROM concessions ORDER BY con_type"); 
 ?><html>
 <head>	
 	<title>Concessions</title>
