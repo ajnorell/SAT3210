d540f59e22d0b937e39703056ea1ff10a7a9d8dc
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-11-30 14:08:08 -0500

    update 179

--- createandload.py
+++ createandload.py
@@ -11,7 +11,7 @@ try:
     con = mariadb.connect(
         user="root",
         password="P@ssw0rd",
-        host="localhost",
+        host="127.0.0.1",
         port=3306,
         local_infile=1
     )
