48a982d25a82edea7b9b737a02bb8cb1bdec3b09
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-11-30 14:37:09 -0500

    update 186

--- add_emp.php
+++ add_emp.php
@@ -89,7 +89,7 @@ else if (isset($_POST['cancel'])) {
 					<input class="cancel" type="submit" name="cancel" value="Cancel">
 				</td>
 				<td>
-					<input type="submit" name="Add" value="update">
+					<input type="submit" name="Add" value="Update">
 				</td>
 			</tr>
 		</table>
