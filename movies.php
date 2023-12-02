10bee869839ae64277109c45a5932d22f62b8631
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-12-02 11:18:57 -0500

    update 223

--- movies.php
+++ movies.php
@@ -14,28 +14,25 @@ include_once("config.php");
 	<link rel="stylesheet" href="styles.css" />
 </head>
 <body>
-    <a>
-    <div style="margin: 0 auto" style="width: 1000 px">
-    <label for="location">Choose a location:</label>
-    <select name="location" id="location" form="form1">
-    <option value="1">Chicago</option>
-    <option value="2">New York</option>
-    <option value="3">Minneapolis</option>
-    <option value="4">Seattle</option>
-    <option value="5">Dallas</option>
-    </select>
-    <br>
-    <form name="form1" method="post" action="displaymovies.php">
-        <label for="month">Month </label>
-        <input type="text" name="month" id="month"><br>
-        <label for="day">Day   </label>
-        <input type="text" name="day" id="day"><br>
-        <input type="submit" name="canacel" value="Cancel">
-        <input type="submit" name="update" value="Search">
-        
-    </form>
-    </div>
-    </a>
+    <table>
+        <form name="form1" method="post" action="displaymovies.php">
+            <label for="location">Choose a location:</label>
+                <select name="location" id="location">
+                <option value="1">Chicago</option>
+                <option value="2">New York</option>
+                <option value="3">Minneapolis</option>
+                <option value="4">Seattle</option>
+                <option value="5">Dallas</option>
+                </select>
+            <br>
+            <label for="date">Date </label>
+                <input type="date" name="date" id="date"><br>
+                
+            <input type="submit" name="canacel" value="Cancel">
+            <input type="submit" name="update" value="Search">
+            
+        </form>
+    </table>
 </body>
 <footer>
 	<a class="buttoncenter" class="button" href="index.php">Home</a>
