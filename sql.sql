e54f13449b9cd7d7c8f749e925aa17238c19edf3
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-12-02 10:57:40 -0500

    update 217

--- sql.sql
+++ sql.sql
@@ -1,7 +1,6 @@
 SELECT movies.title, 
 time_slot.day, time_slot.start_hr, time_slot.start_min, 
 screens.screen_num, screens.seat_type, screens.screen_type, screens.capacity
-FROM movies INNER JOIN screens ON movies.screen_ID = screens.screen_ID
-UNION
-movies INNER JOIN time_slot ON movies.time_slot_ID = time_slot.slot_ID
+FROM 
+time_slot INNER JOIN movies ON movies.time_slot_ID = time_slot.slot_ID INNER JOIN screens ON movies.screen_ID = screens.screen_ID
 WHERE 8 BETWEEN movies.start_date AND movies.end_date AND loc_ID = 1
