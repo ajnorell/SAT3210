f6619e074262c6b8e65083ba03c50038d7e3cae1
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-12-05 13:07:36 -0500

    update 233

--- displaymovies.php
+++ displaymovies.php
@@ -25,7 +25,7 @@ if(isset($_POST['update']))
 		}	
 	} else {	
 		// Execute select 
-		$result = mysqli_query($mysqli, "SELECT movies.title, time_slot.day, time_slot.start_hr, time_slot.start_min, screens.screen_num, screens.seat_type, screens.screen_type, screens.capacity FROM time_slot INNER JOIN movies ON movies.time_slot_ID = time_slot.slot_ID INNER JOIN screens ON movies.screen_ID = screens.screen_ID WHERE $date BETWEEN movies.start_date AND movies.end_date AND movies.loc_ID = $location");
+		$result = mysqli_query($mysqli, "SELECT movies.title, time_slot.day, time_slot.start_hr, time_slot.start_min, screens.screen_num, screens.seat_type, screens.screen_type, screens.capacity FROM time_slot INNER JOIN movies ON time_slot.slot_ID = movies.time_slot_ID INNER JOIN screens ON movies.screen_ID = screens.screen_ID WHERE $date BETWEEN movies.start_date AND movies.end_date AND movies.loc_ID = $location");
 	}}
 
 else if (isset($_POST['cancel'])) {
