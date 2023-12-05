ff0fa6e4ab0f7ab728928a2f2931b6c199a28139
ajnorell <54289182+ajnorell@users.noreply.github.com>
2023-12-05 12:59:59 -0500

    update 231

--- sql.sql
+++ sql.sql
@@ -3,5 +3,4 @@ time_slot.day, time_slot.start_hr, time_slot.start_min,
 screens.screen_num, screens.seat_type, screens.screen_type, screens.capacity,
 FROM time_slot 
 INNER JOIN movies ON time_slot.slot_ID = movies.time_slot_ID
-INNER JOIN screens ON movies.screen_ID = screens.screen_ID
 WHERE 2023-08-15 BETWEEN movies.start_date AND movies.end_date
